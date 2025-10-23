9716921e6c8d0df40429a0d44f2d4d7700945bf7
dhel <dhel@itu.dk>
2025-10-23 14:10:42 +0200

    Stuff
    Pushing,

---
+++ CoyoteApp/Tests/ConcurrentAccessTest.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.Coyote.SystematicTesting;
+using Microsoft.Coyote.SystematicTesting.Frameworks.XUnit;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace CoyoteApp.Tests;
+
+public class ConcurrentAccessTest
+{
+    private readonly ITestOutputHelper _output;
+
+    public ConcurrentAccessTest(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Fact]
+    public async Task TestUnsafePublication()
+    {
+        UnsafePublication obj = null;
+        Task creator = Task.Run(() =>
+        {
+            obj = new UnsafePublication();
+        });
+        Task writer = Task.Run(() =>
+        {
+            while (obj == null);
+            obj.X = 42;
+        });
+        Task reader = Task.Run(() =>
+        {
+            while (obj == null);
+            int value = obj.X;
+            _output.WriteLine($"Read value: {value}");
+            Assert.Equal(42, value);
+
+        });
+        await Task.WhenAll(creator, writer, reader);
+
+        Assert.Equal(42, obj.X);
+    }
+    [Fact]
+    public async Task TestUnsafeIncrement()
+    {
+        UnsafePublication obj = new UnsafePublication();
+        Object someObj = new Object();
+        List<Task> tasks = new List<Task>();
+        for (int i = 0; i < 100; i++)
+        {
+            tasks.Add(Task.Run(() =>
+            {
+                for (int j = 0; j < 1000; j++)
+                {
+                    obj.Increment();
+                }
+            }));
+        }
+        await Task.WhenAll(tasks);
+        _output.WriteLine($"Final value: {obj.X}");
+        Assert.Equal(100000, obj.X);
+    }
+
+    /// <summary>
+    /// Concurrent Coyote test for <see cref="UnsafeInheritance"/>, run with default Configuration.
+    /// </summary>
+    [Test]
+    [Fact(DisplayName = "CoyoteTest_UnsafePub_Inheritance")]
+    public async Task CoyoteTest_UnsafePub_()
+    {
+        var conf = Utils.GetDefaultConfiguration_100();
+        var engine = TestingEngine.Create(conf, TestUnsafePublication);
+        // Ensure Results folder exists
+        var dicInfo = Directory.CreateDirectory("./TestResults/");
+        engine.Run();
+        engine.TryEmitReports(dicInfo.FullName, "test", out IEnumerable<string> reportPaths );
+        _output.WriteLine("Report paths: " + string.Join(", ", reportPaths));
+
+        var reportText = engine.TestReport.GetText(conf, "[UNSAFE_PUB] ");
+
+        Assert.True(engine.TestReport.NumOfFoundBugs == 0, reportText);
+    }
+
+}
+
+public class UnsafePublication
+{
+    private readonly ReaderWriterLockSlim cacheLock = new ReaderWriterLockSlim();
+    private int _x;
+    public int X
+    {
+        get
+        {
+            cacheLock.EnterReadLock();
+            try
+            {
+                return _x;
+            }
+            finally
+            {
+                cacheLock.ExitReadLock();
+            }
+        }
+        set
+        {
+            cacheLock.EnterWriteLock();
+            try
+            {
+                _x = value;
+            }
+            finally
+            {
+                cacheLock.ExitWriteLock();
+            }
+        }
+    }
+
+    public void Increment()
+    {
+        cacheLock.EnterWriteLock();
+        try
+        {
+            _x++;
+        }
+        finally
+        {
+            cacheLock.ExitWriteLock();
+        }
+    }
+}
---
+++ CoyoteApp/Tests/ReorderingTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CoyoteApp.Tests;
+
+public class ReorderingTest
+{
+    [Fact]
+    public void VolatilePreventsReordering()
+    {
+        var outputs = new ConcurrentBag<string>();
+        // Replace Console.WriteLine with a delegate for testability
+        Action<string> output = s => outputs.Add(s);
+
+        // Run Init and Print concurrently many times
+        Parallel.For(0, 10000, _ =>
+        {
+            var obj = new DataInit(output);
+            Parallel.Invoke(
+                () => obj.Init(),
+                () => obj.Print()
+            );
+        });
+
+        // Assert "0" is never printed when initialized
+        foreach (var line in outputs)
+        {
+            Assert.NotEqual("0", line);
+        }
+    }
+}
+
+
+
+/*
+ * The DataInit class in Figure 7 has two methods, Init and Print; both may be called from multiple threads. 
+ * If no memory operations are reordered, Print can only print “Not initialized” or “42,” but there are two possible cases when Print could print a “0”:
+
+    Write 1 and Write 2 were reordered.
+    Read 1 and Read 2 were reordered.
+ *If _initialized were not marked as volatile, both reorderings would be permitted. 
+ *However, when _initialized is marked as volatile, neither reordering is allowed! 
+ *In the case of writes, you have an ordinary write followed by a volatile write, and a volatile write can’t be reordered with a prior memory operation. 
+ *In the case of the reads, you have a volatile read followed by an ordinary read, and a volatile read can’t be reordered with a subsequent memory operation.
+ *
+ * Release-Acquire semantics thus prevent both kinds of reordering in this example.
+ * 
+ * Note that if the _data field is volatile but _initialized is not, both reorderings would be permitted. As a result, remembering this example is a good way to remember the volatile semantics.
+ *
+ */
+
+public class DataInit
+{
+    private int _data = 0;
+    private volatile bool _initialized = false;
+    private readonly Action<string> _output;
+    public DataInit(Action<string> output)
+    {
+        _output = output;
+    }
+    public void Init()
+    {
+        _data = 42;            // Write 1
+        _initialized = true;   // Write 2
+    }
+    public void Print()
+    {
+        if (_initialized)// Read 1
+        {
+            _output(_data.ToString());  // Read 2
+        }
+        else
+        {
+            _output("Not initialized");
+        }
+    }
+}
